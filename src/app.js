4f29c28e3970d2dc2116ba0dc0b6ab1753bb09ec
Hayder10 <rodrigo.cabrera.catemu10@gmail.com>
2022-06-28 17:12:55 +0000

    First version of Base Code

--- src/app.js
+++ src/app.js
@@ -6,6 +6,57 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var card = cardPick();
+  var number = document.getElementById("number");
+  var upperSuit = document.getElementById("upperIcon");
+  var lowerSuit = document.getElementById("lowerIcon");
+  if (card[0] == "heart") {
+    upperSuit.style.color = "red";
+    lowerSuit.style.color = "red";
+    upperSuit.innerHTML = "♥";
+    lowerSuit.innerHTML = "♥";
+    number.innerHTML = card[1];
+  } else if (card[0] == "diamond") {
+    upperSuit.style.color = "red";
+    lowerSuit.style.color = "red";
+    upperSuit.innerHTML = "♦";
+    lowerSuit.innerHTML = "♦";
+    number.innerHTML = card[1];
+  } else if (card[0] == "spade") {
+    upperSuit.innerHTML = "♠";
+    lowerSuit.innerHTML = "♠";
+    number.innerHTML = card[1];
+  } else {
+    upperSuit.innerHTML = "♣";
+    lowerSuit.innerHTML = "♣";
+    number.innerHTML = card[1];
+  }
 };
+
+const suits = ["spade", "heart", "diamond", "clover"];
+const numbers = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+
+function cardPick() {
+  var numberSuit = Math.floor(Math.random() * 4);
+  var numberNum = Math.floor(Math.random() * 13);
+  var result = [];
+
+  result.push(suits[numberSuit]);
+  result.push(numbers[numberNum]);
+  console.log(result);
+  return result;
+}
